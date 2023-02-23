1629769b16255839c8935c376f6db6397a732d11
valefacio <valefacio@valefacio.co>
2023-02-22 22:23:25 -0500

    Update to comments descrbing script.

--- dig-at-nameserver.sh
+++ dig-at-nameserver.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-: 'Written: Monday, July 10, 2017.
-Script receives text file in the, inputs directory, with list of domains as input.
+'
+Script receives text file in the INPUT directory with a list of domains as input.
 Script runs dig query.
-Script appends output of query to file after query for each domain.
-Script renames the output file and moves to the results directory.
+Script appends output of query to the file for each domain.
+Script renames the output file, with a timestamp, and moves to the file to the OUTPUT directory.
 
-Permissions: chmod u+x filename.sh
-Execute: ./filename.sh
+Permissions needed to execute file: chmod u+x filename.sh
+Execute command in terminal while in the same directory as file: ./filename.sh
 '
 
 INPUT="../input/sample-domain-list"
