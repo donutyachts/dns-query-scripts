a806d31af008a8f2c4204d9576867a437c9c540d
donutyachts <95940735+donutyachts@users.noreply.github.com>
2024-05-31 09:49:12 -0400

    Update dig-mail-records.sh
    Updated explanation.

--- dig-mail-records.sh
+++ dig-mail-records.sh
@@ -3,8 +3,8 @@
 '
 Script receives text file in the INPUT directory with a list of domains as input.
 Script runs dig query.
-Script appends output of query to the file for each domain.
-Script renames the output file, with a timestamp, and moves to the file to the OUTPUT directory.
+Script appends output of query to the OUTPUT file for each domain.
+Script renames the OUTPUT file, with a timestamp, and moves to the OUTPUT file to the OUTPUT directory.
 
 Permissions needed to execute file: chmod u+x filename.sh
 Execute command in terminal while in the same directory as file: ./filename.sh
